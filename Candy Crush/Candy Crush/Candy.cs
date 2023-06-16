8d932246e657e31119181807750b4357058632c4
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-06-16 20:31:39 +0330

    Candy class add

---
+++ Candy Crush/Candy Crush/Candy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candy_Crush
+{
+    public class Candy
+    {
+        public string Type { get; set; }
+        public Point Position { get; set; }
+
+        // Constructor
+        public Candy(string type, Point position)
+        {
+            Type = type;
+            Position = position;
+        }
+    }
+}
