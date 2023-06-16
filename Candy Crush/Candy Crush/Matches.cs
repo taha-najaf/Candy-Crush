113fc772ce229dd50d27fabe797a2519984f7362
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-06-16 20:22:06 +0330

    Match class add

---
+++ Candy Crush/Candy Crush/Matches.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candy_Crush
+{
+    public class Matches
+    {
+        public Players Winner { get; set; }
+        public Players Loser { get; set; }
+
+    }
+}
