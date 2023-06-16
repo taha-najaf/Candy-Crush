9f392ee8c44951aeb8eaee9ec2e9d1011b034550
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-06-16 20:53:52 +0330

    Players class update

--- Candy Crush/Candy Crush/Players.cs
+++ Candy Crush/Candy Crush/Players.cs
@@ -8,6 +8,9 @@ namespace Candy_Crush
 {
     public class Players
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
         public static string Username { get; set; }
         public static string Password { get; set; }
         public int record { get ; set; }
@@ -16,6 +19,14 @@ namespace Candy_Crush
         public int Wins { get; set; }
         public int Loses { get; set; }
         public List <Matches> Matches { get; set; }
-
+        public Players() { }
+        public Players( string name, string family,string username,string password)
+        {
+            
+            Name = name;
+            Family = family;
+            Username = username;
+            Password = password;
+        }
     }
 }
