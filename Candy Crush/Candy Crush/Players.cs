ce01b5730a827a071be33bd58d23ca281e45a0a8
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-06-16 20:22:19 +0330

    players class add

---
+++ Candy Crush/Candy Crush/Players.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candy_Crush
+{
+    public class Players
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public int record { get ; set; }
+        public int Number_Of_Games_Played { get; set;}
+        public List <Players> Friends { get; set; }
+        public int Wins { get; set; }
+        public int Loses { get; set; }
+        public List <Matches> Matches { get; set; }
+
+    }
+}
