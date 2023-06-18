1ad08cbc9d82540474e20e2d71a8384e035dab1c
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-06-18 17:46:19 +0330

    Friend class add

---
+++ Candy Crush/Candy Crush/Friends.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candy_Crush
+{
+    public class Friends
+    {
+        public int PlayerID { get; set; }
+        public int FriendPlayerID { get; set; }
+        public string FriendCode { get; set; }
+        public string FriendName { get; set; }
+        public string FriendFamily { get; set; }
+        public int record { get; set; }
+        public int Number_Of_Games_Played { get; set; }
+        public int Wins { get; set; }
+        public int Loses { get; set; }
+    }
+}
--- Candy Crush/Candy Crush/Players.cs
+++ Candy Crush/Candy Crush/Players.cs
@@ -15,7 +15,6 @@ namespace Candy_Crush
         public string Password { get; set; }
         public int record { get ; set; }
         public int Number_Of_Games_Played { get; set;}
-        public List <Players> Friends { get; set; }
         public int Wins { get; set; }
         public int Loses { get; set; }
         public string Code { get; set; }
