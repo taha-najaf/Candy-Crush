4d6823703cf0ecc6517ab04818b6f4d8da9cf1c5
taha-najaf <127234497+taha-najaf@users.noreply.github.com>
2023-06-20 22:36:06 +0330

    Contest application class add

---
+++ Candy Crush/Candy Crush/ContestApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candy_Crush
+{
+    public class ContestApplication
+    {
+        [Key]
+        public int ApplicantId { get; set; }
+        public int FriendId { get; set; }
+        public int UserId { get; set; }
+        
+
+        public ContestApplication(int applicantId, int userId,int friendId)
+        {
+            ApplicantId = applicantId;
+            UserId = userId;
+            FriendId = friendId;
+        }
+    }
+}
